60d13c506bb72fbc683b89a822dd04e7c58f106b
Pavel Dzunovich <pavel_dzunovich@epam.com>
2019-10-07 12:43:45 +0300

    Initialize repository

---
+++ src/test/java/com/segg3r/expencetracker/ExpenceTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.segg3r.expencetracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExpenceTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
